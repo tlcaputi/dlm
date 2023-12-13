58fb0bce8c6a66875318179733846e861eba9cd6
Theodore Caputi <theo@Theodores-MacBook-Pro.local>
2023-12-13 16:29:25 -0500

    fixed cpi

--- R/cpi-functions.R
+++ R/cpi-functions.R
@@ -11,14 +11,16 @@
 #' 
 inflation_adjust = function(df, old_price_var, new_price_var, year_var, to_year){
   data(cpi)
+  baseline_val = cpi$cpi[cpi$year == to_year]
   cpi = cpi %>% 
-      mutate(adjustment_factor = cpi / cpi[year == to_year]) %>%
+      mutate(adjustment_factor = cpi / baseline_val) %>%
       select(
-          year, 
+          "{year_var}" := year, 
           adjustment_factor
-      )
+      ) %>%
+      unique()
   df = setDT(df); cpi = setDT(df)
-  df = merge(df, cpi, by = "year", all.x = T)
+  df = merge(df, cpi, by = year_var, all.x = T)
   df = as.data.frame(df)
   df = df %>% 
     mutate(
